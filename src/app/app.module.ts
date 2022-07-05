581d95ab74f496c876104a119c099494fe3aeeb5
wackynicky <nikhilsekhar4626@gmail.com>
2022-07-05 14:31:45 +0530

    faculty added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DeptnameComponent } from './deptname/deptname.component';
+import { FacultyComponent } from './faculty/faculty.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DeptnameComponent
+    DeptnameComponent,
+    FacultyComponent
   ],
   imports: [
     BrowserModule,
