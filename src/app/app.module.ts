618b24d0ee06a27a09ddfb85c80a01c44eed390e
wackynicky <nikhilsekhar4626@gmail.com>
2022-07-05 14:48:04 +0530

    student added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DeptnameComponent } from './deptname/deptname.component';
 import { FacultyComponent } from './faculty/faculty.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DeptnameComponent,
-    FacultyComponent
+    FacultyComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
